36c02f20e0328442ba09ec8dbfe2d16dec4d4ef4
Nadeflore <flore.love@gmail.com>
2014-04-27 18:33:51 +0200

    Added admin management for every model
    -Updated admin interface to allow addition/edition of every model.
    -Added unicode methods to every models

--- server/music/admin.py
+++ server/music/admin.py
@@ -1,11 +1,61 @@
 from django.contrib import admin
 from music.models import *
 # Register your models here.
-admin.site.register(Language)
+
+
+#Name models
+
+class PersonNameInline(admin.StackedInline):
+    model = PersonName
+    extra = 1
+
+class PersonNameContainerAdmin(admin.ModelAdmin):
+    inlines = [PersonNameInline]
+
+admin.site.register(PersonNameContainer ,PersonNameContainerAdmin)
+
+class ObjectNameInline(admin.TabularInline):
+    model = ObjectName
+    extra = 1
+
+class ObjectNameContainerAdmin(admin.ModelAdmin):
+    inlines = [ObjectNameInline]
+
+admin.site.register(ObjectNameContainer ,ObjectNameContainerAdmin)    
+
+
+#music model
+
+class ArtistMusicInline(admin.TabularInline):
+    model = ArtistMusic
+    extra = 1
+    
+class MusicOpusInline(admin.TabularInline):
+    model = MusicOpus
+    extra = 1
+
+class VideoInline(admin.TabularInline):
+    model = Video
+    extra = 1
+    
+class AudioInline(admin.TabularInline):
+    model = Audio
+    extra = 1
+    
+class SubtitleInline(admin.StackedInline):
+    model = Subtitle
+    extra = 1
+    
+class MusicAdmin(admin.ModelAdmin):
+    inlines = (ArtistMusicInline,MusicOpusInline,VideoInline,AudioInline,SubtitleInline)
+
+
+admin.site.register(Music,MusicAdmin)
+
 admin.site.register(Artist)
+admin.site.register(Language)
 admin.site.register(Role)
 admin.site.register(Opus)
 admin.site.register(MusicOpusType)
-admin.site.register(Music)
 admin.site.register(VideoType)
 admin.site.register(Timer)
--- server/music/models.py
+++ server/music/models.py
@@ -1,44 +1,92 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Language(models.Model):
     name = models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return self.name
 
 #Names structures
     
 class ObjectNameContainer(models.Model):
     id = models.AutoField(primary_key=True)
+    
+    def getMainName(self):
+        main_names = self.objectname_set.filter(isMain=True)
+        if main_names:
+            return main_names[0]
+        else:
+            return None
+    
+    def __unicode__(self):
+        main_name = self.getMainName()
+        if main_name:
+            return unicode(main_name)
+        else:
+            return unicode("")
 
 class ObjectName(models.Model):
     container = models.ForeignKey(ObjectNameContainer)
     name = models.CharField(max_length=200)
-    nameTransliterated = models.CharField(max_length=200)
+    nameTransliterated = models.CharField(max_length=200,blank=True)
     isMain = models.BooleanField()
+    
+    def __unicode__(self):
+        return self.name
 
 class PersonNameContainer(models.Model):
-    id = models.AutoField(primary_key=True) 
+    id = models.AutoField(primary_key=True)    
     
+    def getMainName(self):
+        main_names = self.personname_set.filter(isMain=True)
+        if main_names:
+            return main_names[0]
+        else:
+            return None
+    
+    def __unicode__(self):
+        main_name = self.getMainName()
+        if main_name:
+            return unicode(main_name)
+        else:
+            return unicode("")
+        
+        
 class PersonName(models.Model):
     container = models.ForeignKey(PersonNameContainer)
     name = models.CharField(max_length=200)
-    nameTransliterated = models.CharField(max_length=200)
-    surname = models.CharField(max_length=200)
-    surnameTransliterated = models.CharField(max_length=200)
+    nameTransliterated = models.CharField(max_length=200,blank=True)
+    surname = models.CharField(max_length=200,blank=True)
+    surnameTransliterated = models.CharField(max_length=200,blank=True)
     isMain = models.BooleanField()
+    
+    def __unicode__(self):
+        return self.name + " " + self.surname
     
 #Artist related models
 
 class Artist(models.Model):
     nameContainer = models.OneToOneField(PersonNameContainer)
+    
+    def __unicode__(self):
+        return unicode(self.nameContainer)
 
 class Role(models.Model):
     name = models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return self.name
     
 #Opus (work, media, fiction) related models
     
 class Opus(models.Model):
     nameContainer = models.OneToOneField(ObjectNameContainer)
     language = models.ForeignKey(Language)
-    date = models.DateField()
+    date = models.DateField(null=True,blank=True)
+    
+    def __unicode__(self):
+        return unicode(self.nameContainer)
     
 class MusicOpusType(models.Model):
     nameShort = models.CharField(max_length=200)
@@ -46,21 +94,27 @@ class MusicOpusType(models.Model):
     hasVersion = models.BooleanField()
     hasInterval = models.BooleanField()
     
+    def __unicode__(self):
+        return self.nameLong
+    
 #Music model
 
 class Music(models.Model):
     titleContainer = models.OneToOneField(ObjectNameContainer)
     uses = models.ManyToManyField(Opus,through='MusicOpus')
-    version = models.CharField(max_length=200)
+    version = models.CharField(max_length=200,blank=True)
     isShort = models.BooleanField()
     isRemix = models.BooleanField()
     isCover = models.BooleanField()
-    date = models.DateField()
+    date = models.DateField(null=True,blank=True)
     duration = models.IntegerField()
     artists = models.ManyToManyField(Artist,through='ArtistMusic')
     languages = models.ManyToManyField(Language)
-    note = models.TextField()
+    note = models.TextField(blank=True)
     filePath = models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return unicode(self.titleContainer)
     
     
 #Video file Streams related models
@@ -68,33 +122,48 @@ class VideoType(models.Model):
     name = models.CharField(max_length=200)
     hasOpus = models.BooleanField()
     
+    def __unicode__(self):
+        return self.name
+    
 class Video(models.Model):
     music = models.ForeignKey(Music)
-    realisator = models.CharField(max_length=200)
-    thumbnailPath = models.CharField(max_length=200)
+    realisator = models.CharField(max_length=200,blank=True)
+    thumbnailPath = models.CharField(max_length=200,blank=True)
     type = models.ForeignKey(VideoType)
     opus = models.ForeignKey(Opus,null=True, blank=True, default = None)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200,blank=True)
     channelId = models.IntegerField()
+    
+    def __unicode__(self):
+        return self.description
 
 class Audio(models.Model):
     music = models.ForeignKey(Music)
     isInstrumental = models.BooleanField()
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200,blank=True)
     channelId = models.IntegerField()
+    
+    def __unicode__(self):
+        return self.description
     
 class Timer(models.Model):
     nameContainer = models.OneToOneField(PersonNameContainer)
+    
+    def __unicode__(self):
+        return unicode(self.nameContainer)
 
 class Subtitle(models.Model):
     music = models.ForeignKey(Music)
-    lyrics = models.TextField()
+    lyrics = models.TextField(blank=True)
     timer = models.ForeignKey(Timer,null=True, blank=True, default = None)
-    transliteration = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-#Transformer en channelId si on remuxe les fichiers ? l'import
+    transliteration = models.CharField(max_length=200,blank=True)
+    description = models.CharField(max_length=200,blank=True)
+#Transformer en channelId si on remuxe les fichiers a l'import
     filePath = models.CharField(max_length=200)
 #  public String cleanLyrics(String inputLyrics)
+
+    def __unicode__(self):
+        return self.description
 
 
 
@@ -119,7 +188,7 @@ class MusicOpus(models.Model):
     opus = models.ForeignKey(Opus)
     type = models.ForeignKey(MusicOpusType)
     version = models.IntegerField(null=True, blank=True, default = None)
-    interval = models.CharField(max_length=200)
+    interval = models.CharField(max_length=200,blank=True)
     language = models.ForeignKey(Language,null=True, blank=True, default = None) #null when opus original language
     kind = models.IntegerField(choices=MUSIC_OPUS_KIND)
 
