382561f3257542dc268eef92007311d0feb18c42
Nadeflore <flore.love@gmail.com>
2014-04-30 19:36:11 +0200

    Changed language page into one form

--- server/music/urls.py
+++ server/music/urls.py
@@ -5,7 +5,6 @@ from music import views
 urlpatterns = [
     url(r'^languages/$', views.langIndex, name='langIndex'),
     url(r'^languages/add$', views.langAdd, name='langAdd'),
-    url(r'^languages/edit/(?P<langId>\d+)/$', views.langEdit, name='langEdit'),
     url(r'^languages/del/(?P<langId>\d+)/$', views.langDel, name='langDel'),
 ]
 
--- server/music/views.py
+++ server/music/views.py
@@ -5,27 +5,26 @@ from django.core.urlresolvers import reverse
 from music.models import *
 from music.forms import *
 
-def langIndex(request, error = None):
+def langIndex(request):
     langList = Language.objects.all()
-    
-    langForms = {}
-    for lang in langList:
-        langForms[lang.id] = LanguageForm(instance=lang)
-    
-    form = LanguageForm()
-    return render(request, 'music/lang.html', {'langForms': langForms,'form':form, 'error_message': error })
+    if request.method == 'POST': #form has been submitted, process data
+        langForms = [LanguageForm(request.POST,instance=lang,prefix=str(lang.id)) for lang in langList]
+        newLangForm  = LanguageForm(request.POST)
+        if not newLangForm.is_valid():
+           newLangForm = LanguageForm()
+
+        if all([f.is_valid() for f in langForms]): #check every entry validity
+            [f.save() for f in langForms]
+            if newLangForm.is_valid():
+                newLangForm.save()
+            return HttpResponseRedirect(reverse('langIndex'))
+    else:
+        langForms = [LanguageForm(instance=lang,prefix=str(lang.id)) for lang in langList]
+        newLangForm = LanguageForm() 
+    return render(request, 'music/lang.html', {'langForms': langForms , 'newLangForm' : newLangForm})
 
 def langAdd(request):
     f = LanguageForm(request.POST)
-    if f.is_valid(): 
-        f.save()
-        return HttpResponseRedirect(reverse('langIndex'))
-    else:
-        return langIndex(request, "Name should not be empty")
-
-def langEdit(request,langId):
-    l = get_object_or_404(Language, pk=langId)
-    f = LanguageForm(request.POST,instance=l)
     if f.is_valid(): 
         f.save()
         return HttpResponseRedirect(reverse('langIndex'))
