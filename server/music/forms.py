a9de4d44140d53cd853d43e6d44a1f1a5cbd15c1
Nadeflore <flore.love@gmail.com>
2014-04-30 15:06:46 +0200

    fixed import in forms.py

--- server/music/forms.py
+++ server/music/forms.py
@@ -1,5 +1,5 @@
-from django.forms import ModelForm
-from django.forms import ValidationError
+from django.forms import ModelForm, ValidationError
+from django import forms
 from music.models import *
 
 
