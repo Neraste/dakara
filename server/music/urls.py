0b1b34abfdf26af3cd4075bb229c3924188c11dd
Nadeflore <flore.love@gmail.com>
2014-05-02 20:00:54 +0200

    Added edit/add pages for Role,usetype,opustype,videotype

--- server/music/urls.py
+++ server/music/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 
 from music import views
+from music.models import *
 
 urlpatterns = [
-    url(r'^languages/$', views.langIndex, name='langIndex'),
-    url(r'^languages/add$', views.langAdd, name='langAdd'),
-    url(r'^languages/del/(?P<langId>\d+)/$', views.langDel, name='langDel'),
+    url(r'^languages/$', views.multiEdit, {'Model': Language} ),
+    url(r'^roles/$', views.multiEdit, {'Model': Role} ),
+    url(r'^usetypes/$', views.multiEdit, {'Model': MusicOpusType} ),
+    url(r'^opustypes/$', views.multiEdit, {'Model': OpusType} ),
+    url(r'^videotypes/$', views.multiEdit, {'Model': VideoType} ),
 ]
 
--- server/music/views.py
+++ server/music/views.py
@@ -6,27 +6,15 @@ from django.core.urlresolvers import reverse
 from music.models import *
 from music.forms import *
 
-def langIndex(request):
-    LanguageFormSet = modelformset_factory(Language) 
+
+def multiEdit(request,Model):
+    FormSet = modelformset_factory(Model) 
     if request.method == 'POST': #form has been submitted, process data
-        langFormSet = LanguageFormSet(request.POST)
-        if langFormSet.is_valid():
-            langFormSet.save()
-            return HttpResponseRedirect(reverse('langIndex'))
+        fSet = FormSet(request.POST)
+        if fSet.is_valid():
+            fSet.save()
+            return HttpResponseRedirect(request.get_full_path() )
     else:
-        langFormSet = LanguageFormSet()
-    return render(request, 'music/lang.html', {'langFormSet': langFormSet})
+        fSet = FormSet()
+    return render(request, 'music/multiedit.html', {'formSet': fSet})
 
-def langAdd(request):
-    f = LanguageForm(request.POST)
-    if f.is_valid(): 
-        f.save()
-        return HttpResponseRedirect(reverse('langIndex'))
-    else:
-        return langIndex(request, "Name should not be empty")
-
-def langDel(request,langId):
-    l = get_object_or_404(Language, pk=langId)
-    l.delete()
-    
-    return HttpResponseRedirect(reverse('langIndex'))
