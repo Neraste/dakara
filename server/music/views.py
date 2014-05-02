fcce98b6c1c219a1b93bf0a112065086a1cabc03
Nadeflore <flore.love@gmail.com>
2014-05-02 18:32:59 +0200

    Use formset in language view and template

--- server/music/views.py
+++ server/music/views.py
@@ -1,3 +1,4 @@
+from django.forms.models import modelformset_factory
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
@@ -6,22 +7,15 @@ from music.models import *
 from music.forms import *
 
 def langIndex(request):
-    langList = Language.objects.all()
+    LanguageFormSet = modelformset_factory(Language) 
     if request.method == 'POST': #form has been submitted, process data
-        langForms = [LanguageForm(request.POST,instance=lang,prefix=str(lang.id)) for lang in langList]
-        newLangForm  = LanguageForm(request.POST)
-        if not newLangForm.is_valid():
-           newLangForm = LanguageForm()
-
-        if all([f.is_valid() for f in langForms]): #check every entry validity
-            [f.save() for f in langForms]
-            if newLangForm.is_valid():
-                newLangForm.save()
+        langFormSet = LanguageFormSet(request.POST)
+        if langFormSet.is_valid():
+            langFormSet.save()
             return HttpResponseRedirect(reverse('langIndex'))
     else:
-        langForms = [LanguageForm(instance=lang,prefix=str(lang.id)) for lang in langList]
-        newLangForm = LanguageForm() 
-    return render(request, 'music/lang.html', {'langForms': langForms , 'newLangForm' : newLangForm})
+        langFormSet = LanguageFormSet()
+    return render(request, 'music/lang.html', {'langFormSet': langFormSet})
 
 def langAdd(request):
     f = LanguageForm(request.POST)
