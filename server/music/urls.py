6eb434020d3bf717d3ef1c810d1b6eff2766ed1a
Nadeflore <flore.love@gmail.com>
2014-05-09 22:50:55 +0200

    Delete and merge fully functional

--- server/music/urls.py
+++ server/music/urls.py
@@ -14,6 +14,11 @@ urlpatterns = [
     url(r'^usetypes/(?P<id>\d+)/$', views.multi_delete, {'Model': MusicOpusType} ),
     url(r'^opustypes/(?P<id>\d+)/$', views.multi_delete, {'Model': OpusType} ),
     url(r'^videotypes/(?P<id>\d+)/$', views.multi_delete, {'Model': VideoType} ),
+    url(r'^languages/(?P<id>\d+)/merge/$', views.multi_merge, {'Model': Language} ),
+    url(r'^roles/(?P<id>\d+)/merge/$', views.multi_merge, {'Model': Role} ),
+    url(r'^usetypes/(?P<id>\d+)/merge/$', views.multi_merge, {'Model': MusicOpusType} ),
+    url(r'^opustypes/(?P<id>\d+)/merge/$', views.multi_merge, {'Model': OpusType} ),
+    url(r'^videotypes/(?P<id>\d+)/merge/$', views.multi_merge, {'Model': VideoType} ),
 
 ]
 
--- server/utils.py
+++ server/utils.py
@@ -1,9 +1,14 @@
 def get_related(obj):
+    has_related = False
     related = {}
     for rel in obj._meta.get_all_related_objects():
         objects = getattr(obj, rel.get_accessor_name() ).all()
-        related[rel.var_name] = objects 
+        if len(objects) != 0:
+            has_related = True
+        related[rel] = objects 
     for rel in obj._meta.get_all_related_many_to_many_objects():
         objects = getattr(obj, rel.get_accessor_name() ).all()
-        related[rel.var_name] = objects 
-    return related
+        if len(objects) != 0:
+            has_related = True
+        related[rel] = objects 
+    return related if has_related else None
