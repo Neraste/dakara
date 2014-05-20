26a5cd0c341e153ce94a51f9a9c091eca0a68ef3
Neraste <neraste.herr10@gmail.com>
2014-05-20 16:44:55 +0200

    URL update for new people

--- server/music/urls.py
+++ server/music/urls.py
@@ -24,7 +24,7 @@ for mod in multi_edit_models:
 for mod in people_models:
     objects = make_name_lower(mod) + 's'
     urlpatterns.append( url( r'^' + objects + r'/$' , views.people_list , {'Model' : mod }, name = objects ) )
-    urlpatterns.append( url( r'^' + objects + r'/new$' , views.people_new , {'Model' : mod } ) )
+    urlpatterns.append( url( r'^' + objects + r'/new$' , views.people_new , {'Model' : mod }, name = objects + '_new' ) )
     urlpatterns.append( url( r'^' + objects + r'/(?P<id>\d+)/$' , views.people_detail_delete , {'Model' : mod }, name = objects + '_detail_delete' ) )
     urlpatterns.append( url( r'^' + objects + r'/(?P<id>\d+)/edit/$' , views.people_edit , {'Model' : mod }, name = objects + '_edit' ) )
 
