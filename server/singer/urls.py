2e36dab730193eea0f4c2fe55e201b3d84fd2c7e
Neraste <neraste.herr10@gmail.com>
2014-07-14 19:05:43 +0200

    Singer app urls and empty views

--- server/singer/urls.py
+++ server/singer/urls.py
@@ -3,4 +3,9 @@ from django.conf.urls import url
 from singer import views
 
 urlpatterns = [
-        ]
+    url(r'^/$', 'singer_list', name = 'users_list'),
+    url(r'^new/$', 'singer_new', name = 'user_new'),
+    url(r'^(?P<id>\d+)/$', 'singer_detail_delete', name = 'user_detail_delete'),
+    url(r'^(?P<id>\d+)/edit/$', 'singer_edit', name = 'user_edit'),
+    url(r'^search/$', 'singer_search', name = 'user_search'),
+    ]
--- server/singer/views.py
+++ server/singer/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from singer.models import *
+
+# Singer area #################################################################
+
+def singer_liset(request):
+    pass
+
+def singer_new(request):
+    pass
+
+def singer_detail_delete(request, id):
+    pass
+
+def singer_edit(request, id):
+    pass
+
+def singer_search(request):
+    pass
+
+# Favourite area ##############################################################
+
+def favourite_list(request):
+    pass
+
+def favourite_add(request, id):
+    pass
+
+def favourite_edit(request, id):
+    pass
+
+def favourite_remove(request, id):
+    pass
