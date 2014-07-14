55798a6b42c4b3c5c56ec628bcccfd995e1dfe11
Neraste <neraste.herr10@gmail.com>
2014-07-14 19:05:00 +0200

    Singer app fixed

--- server/singer/admin.py
+++ server/singer/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
+
 from singer.models import *
+from name.admin import *
 
 class FavouriteInline(admin.TabularInline):
     model = MusicSinger
     extra = 1
 
 class SingerAdmin(admin.ModelAdmin):
-    inlines = [FavouriteInline]
-    fields = ('username', 'password', 'email', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+    inlines = (FavouriteInline,)
+    fields = ('username', 'password', 'email', 'person', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
 
 admin.site.register(Singer, SingerAdmin)
--- server/singer/models.py
+++ server/singer/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 from music.models import Music
+from name.models import *
 
 class Singer(AbstractUser):
+    person = models.OneToOneField(Person, null = True, blank = True)
     favourites = models.ManyToManyField(Music, through = 'MusicSinger')
 
+    def __unicode__(self):
+        return unicode(self.person) if self.person else self.username
+
     class Meta:
-        verbose_name = "singer"
-        verbose_name_plural = "singers"
+        verbose_name = "user"
+        verbose_name_plural = "users"
 
 class MusicSinger(models.Model):
     singer = models.ForeignKey(Singer)
     music = models.ForeignKey(Music)
-    grade = models.IntegerField()
+    grade = models.PositiveSmallIntegerField()
     
     class Meta:
         verbose_name = "favourite"
